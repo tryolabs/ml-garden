a3432c33cbedc0e32ece5baf71257b19cbfaef24
Diego Marvid <dmarvid@tryolabs.com>
2024-03-27 14:20:12 -0300

    refactor Optuna into a new class

--- pipeline_lib/core/steps/fit_model.py
+++ pipeline_lib/core/steps/fit_model.py
@@ -1,19 +1,88 @@
 import json
+import logging
 from typing import Optional, Type
 
 import optuna
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 from pipeline_lib.core import DataContainer
 from pipeline_lib.core.model import Model
 from pipeline_lib.core.steps.base import PipelineStep
-from pipeline_lib.core.utils import calculate_error
+
+
+class OptunaOptimizer:
+    def __init__(self, optuna_params: dict, logger: logging.Logger) -> None:
+        self.optuna_params = optuna_params
+        self.logger = logger
+
+    def optimize(
+        self, X_train, y_train, X_valid, y_valid, model_class: Type[Model], model_params: dict
+    ) -> dict:
+        def objective(trial):
+            param = self._create_trial_params(trial)
+            param.update(model_params)
+
+            model = model_class(**param)
+            model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=False)
+            preds = model.predict(X_valid)
+
+            objective_metric = self.optuna_params.get("objective_metric", "mean_absolute_error")
+            error = self._calculate_error(y_valid, preds, objective_metric)
+            return error
+
+        study = self._create_study()
+        study.optimize(objective, n_trials=self.optuna_params.get("trials", 20))
+
+        best_params = study.best_params
+        return best_params
+
+    def _create_trial_params(self, trial) -> dict:
+        param = {}
+        for key, value in self.optuna_params.get("search_space", {}).items():
+            if isinstance(value, dict):
+                suggest_func = getattr(trial, f"suggest_{value['type']}")
+                kwargs = value.get("kwargs", {})
+                param[key] = suggest_func(key, *value["args"], **kwargs)
+            else:
+                param[key] = value
+        return param
+
+    def _create_study(self) -> optuna.Study:
+        study_name = self.optuna_params.get("study_name")
+        storage = self.optuna_params.get("storage", "sqlite:///db.sqlite3")
+        load_if_exists = self.optuna_params.get("load_if_exists", False)
+
+        self.logger.info(
+            f"Creating Optuna study with parameters: \n {json.dumps(self.optuna_params)}"
+        )
+
+        study = optuna.create_study(
+            study_name=study_name,
+            storage=storage,
+            load_if_exists=load_if_exists,
+            direction="minimize",
+        )
+        return study
+
+    @staticmethod
+    def _calculate_error(y_true, y_pred, metric):
+        metrics = {
+            "mean_absolute_error": mean_absolute_error,
+            "mean_squared_error": mean_squared_error,
+            "root_mean_squared_error": lambda y_true, y_pred: mean_squared_error(
+                y_true, y_pred, squared=False
+            ),
+        }
+
+        if metric not in metrics:
+            raise ValueError(f"Unsupported objective metric: {metric}")
+
+        return metrics[metric](y_true, y_pred)
 
 
 class FitModelStep(PipelineStep):
-    """Fit the model."""
-
-    used_for_prediction = False
     used_for_training = True
+    used_for_prediction = False
 
     def __init__(
         self,
@@ -22,10 +91,8 @@ class FitModelStep(PipelineStep):
         model_params: Optional[dict] = None,
         drop_columns: Optional[list[str]] = None,
         optuna_params: Optional[dict] = None,
-        search_space: Optional[dict] = None,
         save_path: Optional[str] = None,
     ) -> None:
-        """Initialize FitModelStep."""
         super().__init__()
         self.init_logger()
         self.model_class = model_class
@@ -33,13 +100,39 @@ class FitModelStep(PipelineStep):
         self.model_params = model_params or {}
         self.drop_columns = drop_columns or []
         self.optuna_params = optuna_params
-        self.search_space = search_space or {}
         self.save_path = save_path
 
     def execute(self, data: DataContainer) -> DataContainer:
-        """Execute the step."""
         self.logger.info(f"Fitting the {self.model_class.__name__} model")
 
+        df_train, df_valid = self._prepare_data(data)
+        X_train, y_train, X_valid, y_valid = self._split_data(df_train, df_valid)
+
+        model_params = self.model_params
+
+        if self.optuna_params:
+            optimizer = OptunaOptimizer(self.optuna_params, self.logger)
+            model_params = optimizer.optimize(
+                X_train, y_train, X_valid, y_valid, self.model_class, model_params
+            )
+            model_params.update(self.model_params)
+            self.logger.info(f"Optimized model parameters: \n{json.dumps(model_params)}")
+            self.logger.info("Re-fitting the model with optimized parameters")
+
+        model = self.model_class(**model_params)
+        model.fit(X_train, y_train, eval_set=[(X_valid, y_valid)], verbose=True)
+
+        data.model = model
+        data.target = self.target
+        data._drop_columns = self.drop_columns
+
+        if self.save_path:
+            self.logger.info(f"Saving the model to {self.save_path}")
+            model.save(self.save_path)
+
+        return data
+
+    def _prepare_data(self, data: DataContainer) -> tuple:
         df_train = data.train
         df_valid = data.validation
 
@@ -47,109 +140,13 @@ class FitModelStep(PipelineStep):
             df_train = df_train.drop(columns=self.drop_columns)
             df_valid = df_valid.drop(columns=self.drop_columns)
 
+        return df_train, df_valid
+
+    def _split_data(self, df_train, df_valid) -> tuple:
         X_train = df_train.drop(columns=[self.target])
         y_train = df_train[self.target]
 
         X_valid = df_valid.drop(columns=[self.target])
         y_valid = df_valid[self.target]
 
-        params = self.model_params
-
-        if self.optuna_params:
-            objective_metric = self.optuna_params.get("objective_metric", "mean_absolute_error")
-            params = self.optimize_with_optuna(X_train, y_train, X_valid, y_valid, objective_metric)
-            data.tuning_params = params
-            self.logger.info(
-                "Finished optimizing the model with Optuna. Using the best parameters to fit the"
-                " model."
-            )
-            # add model_params to params
-            params.update(self.model_params)
-
-        self.logger.info(
-            f"Fitting the model with the following parameters: \n {json.dumps(params, indent=4)}"
-        )
-        self.model = self.model_class(**params)
-
-        self.model.fit(
-            X_train,
-            y_train,
-            eval_set=[(X_valid, y_valid)],
-            verbose=True,
-        )
-
-        data.model = self.model
-        data.target = self.target
-        data._drop_columns = self.drop_columns
-
-        if self.save_path:
-            self.logger.info(f"Saving the model to {self.save_path}")
-            self.model.save(self.save_path)
-
-        return data
-
-    def optimize_with_optuna(self, X_train, y_train, X_valid, y_valid, objective_metric: str):
-        def objective(trial):
-            param = {}
-            for key, value in self.search_space.items():
-                if isinstance(value, dict):
-                    suggest_func = getattr(trial, f"suggest_{value['type']}")
-                    kwargs = value.get("kwargs", {})
-                    param[key] = suggest_func(key, *value["args"], **kwargs)
-                else:
-                    param[key] = value
-
-            # add model_params to params
-            param.update(self.model_params)
-
-            model = self.model_class(**param)
-            model.fit(
-                X_train,
-                y_train,
-                eval_set=[(X_valid, y_valid)],
-                verbose=True,
-            )
-            preds = model.predict(X_valid)
-
-            error = calculate_error(y_valid, preds, objective_metric)
-
-            return error
-
-        def optuna_logging_callback(study, trial):
-            if trial.state == optuna.trial.TrialState.COMPLETE:
-                self.logger.info(
-                    f"Trial {trial.number} finished with value: {trial.value} and parameters:"
-                    f" {trial.params}. Best is trial {study.best_trial.number} with value:"
-                    f" {study.best_value}."
-                )
-
-        if not self.optuna_params:
-            raise ValueError("Optuna parameters are not provided.")
-
-        optuna_trials = self.optuna_params.get("trials", 20)
-
-        self.logger.info(f"Optimizing XGBoost hyperparameters with {optuna_trials} trials.")
-
-        study_name = self.optuna_params.get("study_name")
-
-        if not study_name:
-            raise ValueError("Study name is not provided in the optuna parameters.")
-
-        load_if_exists = self.optuna_params.get("load_if_exists", False)
-        if load_if_exists:
-            self.logger.info("Loading existing study if it exists. It will overwrite the study.")
-
-        storage = self.optuna_params.get("storage", "sqlite:///db.sqlite3")
-
-        study = optuna.create_study(
-            direction="minimize",
-            study_name=study_name,
-            storage=storage,
-            load_if_exists=load_if_exists,
-        )
-
-        study.optimize(objective, n_trials=optuna_trials, callbacks=[optuna_logging_callback])
-
-        best_params = study.best_params
-        self.logger.info(f"Best parameters found by Optuna: \n{json.dumps(best_params, indent=4)}")
-        return best_params
+        return X_train, y_train, X_valid, y_valid
