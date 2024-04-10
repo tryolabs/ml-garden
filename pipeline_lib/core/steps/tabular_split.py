dd92c9ea0241f41197caef9b5361bce8d4aa3d3a
Pablo Alfaro <pabloalfaropineyro@gmail.com>
2024-04-10 10:57:20 -0300

    Address PR comments Fix a bug in the random split flow

--- pipeline_lib/core/steps/tabular_split.py
+++ pipeline_lib/core/steps/tabular_split.py
@@ -13,33 +13,41 @@ def _concatenate_columns(
     sep: str = " ",
     na_rep: str = "",
 ) -> pd.Series:
-    """Concatenates the specified `cols` of a DataFrame into a single column.
+    """
+    Concatenates the specified `cols` of a DataFrame into a single column.
 
     This function fills NaN values with a specified string, converts the columns to string type,
     concatenates them with a specified separator and returns the concatenated values.
 
-    Example:
-        >>> df = pd.DataFrame({
-        ...     'A': ['a', 'b', np.nan],
-        ...     'B': ['c', np.nan, 'd'],
-        ...     'C': ['e', 'f', 'g']
-        ... })
-        >>> concatenate_columns(df, ['A', 'B', 'C'])
-           D
-        0  a c e
-        1  b f
-        2  d g
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame to process.
+    cols : iterable of str or None, optional
+        An iterable of column names to concatenate. If None, will choose all columns like `into_col`
+        with `df.filter`. Default is None.
+    sep : str, optional
+        The separator to use when concatenating. Default is " ".
+    na_rep : str, optional
+        The string to use to replace NaN values. Default is "".
 
-    Args:
-        df (pd.DataFrame): The DataFrame to process.
-        cols (Iterable[str] | None, optional): An iterable of column names to concatenate. If None,
-            will chose all columns like `into_col` with `df.filter`. Defaults to None.
-        sep (str, optional): The separator to use when concatenating. Defaults to " ".
-        na_rep (str, optional): The string to use to replace NaN values. Defaults to "".
+    Returns
+    -------
+    pandas.DataFrame
+        The processed DataFrame, which includes the new column and excludes the original columns.
 
-    Returns:
-        pd.DataFrame: The processed DataFrame, which includes the new column and excludes the
-            original columns.
+    Examples
+    --------
+    >>> df = pd.DataFrame({
+    ...     'A': ['a', 'b', np.nan],
+    ...     'B': ['c', np.nan, 'd'],
+    ...     'C': ['e', 'f', 'g']
+    ... })
+    >>> concatenate_columns(df, ['A', 'B', 'C'])
+       D
+    0  a c e
+    1  b f
+    2  d g
     """
     to_concat = df[cols].fillna(na_rep).astype(str)
     concatted = to_concat.iloc[:, 0].str.cat(others=to_concat.iloc[:, 1:], sep=sep)
@@ -130,6 +138,31 @@ class TabularSplitStep(PipelineStep):
                 test_df = df[concatted_groupby_columns.isin(set(test_values))]
             else:
                 test_df = None
+        else:
+            train_df = df[df.index.isin(set(train_values))]
+            validation_df = df[df.index.isin(set(validation_values))]
+            if test_values:
+                test_df = df[df.index.isin(set(test_values))]
+
+        if self.group_by_columns is not None:
+            train_groups = len(train_values)
+            validation_groups = len(validation_values)
+            test_groups = len(test_values) if test_values is not None else 0
+            total_groups = train_groups + validation_groups + test_groups
+
+            self.logger.info(f"Using group by columns for splits based on: {self.group_by_columns}")
+            self.logger.info(
+                f"Number of groups in train set: {train_groups} | {train_groups / total_groups:.2%}"
+            )
+            self.logger.info(
+                f"Number of groups in validation set: {validation_groups} |"
+                f" {validation_groups / total_groups:.2%}"
+            )
+            if test_df is not None:
+                self.logger.info(
+                    f"Number of groups in test set: {test_groups} |"
+                    f" {test_groups / total_groups:.2%}"
+                )
 
         train_rows = len(train_df)
         validation_rows = len(validation_df)
