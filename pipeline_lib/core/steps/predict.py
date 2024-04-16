f1f502a0ae7ba383f1a3c411db6b034fa1927eba
Diego Marvid <dmarvid@tryolabs.com>
2024-04-16 10:11:52 -0300

    enable prediction by mflow model link

--- pipeline_lib/core/steps/predict.py
+++ pipeline_lib/core/steps/predict.py
@@ -1,3 +1,7 @@
+from typing import Optional
+
+import mlflow.pyfunc
+
 from pipeline_lib.core import DataContainer
 from pipeline_lib.core.steps.base import PipelineStep
 
@@ -10,16 +14,27 @@ class PredictStep(PipelineStep):
 
     def __init__(
         self,
+        mlflow_model_name: Optional[str] = None,
+        mlflow_model_version: Optional[int] = None,
     ) -> None:
         """Initialize Predict Step."""
         super().__init__()
         self.init_logger()
+        self.mlflow_model_name = mlflow_model_name
+        self.mlflow_model_version = mlflow_model_version
+
+        # ensure if model name is provided version exist and vice versa
+        if self.mlflow_model_name and not self.mlflow_model_version:
+            raise ValueError("Model version must be provided if model name is provided.")
+
+        if not self.mlflow_model_name and self.mlflow_model_version:
+            raise ValueError("Model name must be provided if model version is provided.")
 
     def execute(self, data: DataContainer) -> DataContainer:
         """Execute the step."""
         self.logger.info("Obtaining predictions")
 
-        if not data.model:
+        if not data.model and not self.mlflow_model_name:
             raise ValueError("Model is not present on the data container.")
 
         if data.is_train:
