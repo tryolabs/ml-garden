bd988e8c82b599812b0bd6d236d1a7c87935b3cc
ovejabu <eliabudelli@gmail.com>
2024-04-23 16:56:54 -0300

    add grouping by different criteria

--- pipeline_lib/core/steps/ocf_reports.py
+++ pipeline_lib/core/steps/ocf_reports.py
@@ -1,10 +1,8 @@
-import matplotlib
+import matplotlib.colors
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
 
 from pipeline_lib.core import DataContainer
-from pipeline_lib.core.model import Model
 from pipeline_lib.core.steps.base import PipelineStep
 
 # setting Tryo colors to use in plots
@@ -12,15 +10,6 @@ from pipeline_lib.core.steps.base import PipelineStep
 # primary Tryo colors
 matplotlib.colors.ColorConverter.colors["tryo_eucalyptus"] = "#2CDDB3"
 matplotlib.colors.ColorConverter.colors["tryo_ultramarine"] = "#4056F4"
-
-# secondary Tryo colors
-matplotlib.colors.ColorConverter.colors["tryo_navy_blue"] = "#1709FF"
-matplotlib.colors.ColorConverter.colors["tryo_blue_violet"] = "#6207F3"
-matplotlib.colors.ColorConverter.colors["tryo_carmine_pink"] = "#FF3139"
-
-# neutral Tryo colors (gray, black)
-matplotlib.colors.ColorConverter.colors["tryo_bright_gray"] = "#E5EAEF"
-matplotlib.colors.ColorConverter.colors["tryo_french_middle"] = "#0B0723"
 
 
 class ocfReportsStep(PipelineStep):
@@ -34,48 +23,44 @@ class ocfReportsStep(PipelineStep):
         super().__init__()
         self.init_logger()
 
-    def _grouped_mae(self, df):
-        grouped_mae_results = []
+    def _calculate_abs_errors(self, y_true, y_pred):
+        abs_errors = abs(y_true - y_pred)
+        return abs_errors
 
-        for month in df["date_month"].unique():
-            df_month = df[df["date_month"] == month]
-            y_true = df_month["average_power_kw"]
-            y_pred = df_month["predictions"]
-            av_gen = y_true.mean()
-            mae = mean_absolute_error(y_true, y_pred)
-            grouped_mae_results.append((month, av_gen, mae))
+    def _group_errors(self, df, target_column, criterion):
+        average_error = df.groupby(criterion)["abs_error"].mean()
+        average_value = df.groupby(criterion)[target_column].mean()
+        grouped_errors = pd.merge(average_error, average_value, on=criterion)
+        return grouped_errors
 
-        mae_df = pd.DataFrame(grouped_mae_results, columns=["date_month", "av_generation", "MAE"])
-
-        return mae_df
-
-    def _plot_grouped_mae(self, df):
+    def _plot_grouped_mae(self, df, target_column, criterion):
         # Plot for Average Generation and Average Error
         bar_width = 0.35
         index = range(df.shape[0])
         plt.figure(figsize=(30, 20))
         plt.rcParams.update({"font.size": 20})
 
-        bar1 = plt.bar(
+        plt.bar(
             index,
-            df["av_generation"],
+            df[target_column],
             bar_width,
-            label="Average generation (kW)",
+            label=f"{target_column}",
             color="tryo_eucalyptus",
         )
-        bar2 = plt.bar(
+        plt.bar(
             [i + bar_width for i in index],
-            df["MAE"],
+            df["abs_error"],
             bar_width,
-            label="MAE (kW)",
+            label=f"MAE by {criterion}",
             color="tryo_ultramarine",
         )
 
-        plt.xlabel("Month", fontsize=30)
-        plt.ylabel("Generation/MAE (kW)", fontsize=30)
-        plt.title("MAE by month", fontsize=40)
-
-        plt.xticks([i + bar_width / 2 for i in index], df["date_month"], rotation=45)
+        plt.xlabel(f"{criterion}", fontsize=30)
+        plt.title(f"MAE by {criterion}", fontsize=40)
+        if len(index) > 500:
+            plt.xticks([])
+        else:
+            plt.xticks([i + bar_width / 2 for i in index], df.index, rotation=45)
         plt.legend(fontsize=30)
 
         plt.show()
@@ -85,9 +70,10 @@ class ocfReportsStep(PipelineStep):
         target_column_name = data.target
         if target_column_name is None:
             raise ValueError("Target column not found on any configuration.")
-
-        df = data.flow[["date_month", target_column_name]]
+        df = data.flow
         df["predictions"] = data.predictions
-        mae_df = self._grouped_mae(df)
-        self._plot_grouped_mae(mae_df)
+        df["abs_error"] = self._calculate_abs_errors(df[target_column_name], df["predictions"])
+        for criterion in ["date_month", "ss_id"]:
+            mae_df = self._group_errors(df, target_column_name, criterion)
+            self._plot_grouped_mae(mae_df, target_column_name, criterion)
         return data
