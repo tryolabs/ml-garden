811ef53e660bba41b3d873fc5c2c8028ef82d2a4
ovejabu <eliabudelli@gmail.com>
2024-04-23 18:20:26 -0300

    add report generation for training

--- pipeline_lib/core/steps/ocf_reports.py
+++ pipeline_lib/core/steps/ocf_reports.py
@@ -1,8 +1,11 @@
+from typing import List, Optional
+
 import matplotlib.colors
 import matplotlib.pyplot as plt
 import pandas as pd
 
 from pipeline_lib.core import DataContainer
+from pipeline_lib.core.model import Model
 from pipeline_lib.core.steps.base import PipelineStep
 
 # setting Tryo colors to use in plots
@@ -16,12 +19,18 @@ class ocfReportsStep(PipelineStep):
     """Generate reports."""
 
     used_for_prediction = True
-    used_for_training = False
+    used_for_training = True
 
     def __init__(self) -> None:
-        """Initialize CalculateMetricsStep."""
+        """Initialize ocfReportStep."""
         super().__init__()
         self.init_logger()
+
+    def _get_predictions(
+        self, model: Model, df: pd.DataFrame, target: str, drop_columns: Optional[List[str]] = None
+    ) -> pd.Series:
+        drop_columns = (drop_columns or []) + [target]
+        return model.predict(df.drop(columns=drop_columns))
 
     def _calculate_abs_errors(self, y_true, y_pred):
         abs_errors = abs(y_true - y_pred)
@@ -65,15 +74,39 @@ class ocfReportsStep(PipelineStep):
 
         plt.show()
 
+    def _generate_report(self, df, target_column_name):
+        df["abs_error"] = self._calculate_abs_errors(df[target_column_name], df["predictions"])
+        for criterion in ["date_month", "ss_id"]:
+            mae_df = self._group_errors(df, target_column_name, criterion)
+            self._plot_grouped_mae(mae_df, target_column_name, criterion)
+
     def execute(self, data: DataContainer) -> DataContainer:
         self.logger.debug("Starting report generation")
         target_column_name = data.target
         if target_column_name is None:
             raise ValueError("Target column not found on any configuration.")
-        df = data.flow
-        df["predictions"] = data.predictions
-        df["abs_error"] = self._calculate_abs_errors(df[target_column_name], df["predictions"])
-        for criterion in ["date_month", "ss_id"]:
-            mae_df = self._group_errors(df, target_column_name, criterion)
-            self._plot_grouped_mae(mae_df, target_column_name, criterion)
+
+        if data.is_train:
+            for dataset_name in ["train", "validation", "test"]:
+                self.logger.info(f"Generating report for {dataset_name}")
+                df = getattr(data, dataset_name, None)
+
+                if df is None:
+                    self.logger.warning(
+                        f"Dataset '{dataset_name}' not found. Skipping metric calculation."
+                    )
+                    continue
+
+                df["predictions"] = self._get_predictions(
+                    model=data.model,
+                    df=df,
+                    target=target_column_name,
+                    drop_columns=data._drop_columns,
+                )
+                self._generate_report(df, target_column_name)
+
+        else:
+            df = data.flow
+            df["predictions"] = data.predictions
+            self._generate_report(df, target_column_name)
         return data
