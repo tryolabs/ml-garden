801520cb163c7ddceebe18088aa378785ef5e046
Pablo Alfaro <pabloalfaropineyro@gmail.com>
2024-04-23 14:30:32 -0300

    Calculate metrics and shap values in training

--- pipeline_lib/core/steps/explainer_dashboard.py
+++ pipeline_lib/core/steps/explainer_dashboard.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from explainerdashboard import RegressionExplainer
 
 from pipeline_lib.core import DataContainer
@@ -7,17 +8,25 @@ from pipeline_lib.core.steps.base import PipelineStep
 class ExplainerDashboardStep(PipelineStep):
     """Scale the target using Quantile Transformer."""
 
-    used_for_prediction = True
-    used_for_training = False
+    used_for_prediction = False
+    used_for_training = True
 
     def __init__(
         self,
-        max_samples: int = 1000,
+        max_samples: int = 100,
+        X_background_samples: int = 100,
+        enable_step: bool = True,
     ) -> None:
         self.init_logger()
         self.max_samples = max_samples
+        self.X_background_samples = X_background_samples
+        self.enable_step = enable_step
 
     def execute(self, data: DataContainer) -> DataContainer:
+        if not self.enable_step:
+            self.logger.info("ExplainerDashboardStep disabled, skipping execution")
+            return data
+
         self.logger.debug("Starting explainer dashboard")
 
         model = data.model
@@ -28,38 +37,53 @@ class ExplainerDashboardStep(PipelineStep):
         if target is None:
             raise ValueError("Target column not found in any parameter.")
 
-        if target not in data.flow.columns:
-            raise ValueError(
-                f"Target column `{target}` not found in the dataset. It must be present for the"
-                " explainer dashboard."
+        if data.is_train:
+            df = data.train
+            if data.validation is not None:
+                df = pd.concat([df, data.validation])
+            if data.test is not None:
+                df = pd.concat([df, data.test])
+
+            if target not in df.columns:
+                raise ValueError(
+                    f"Target column `{target}` not found in the dataset. It must be present for the"
+                    " explainer dashboard."
+                )
+
+            # Some Shap explainers require a "background dataset" with the original distribution
+            # of the data.
+            if self.X_background_samples > 0 and len(df) > self.X_background_samples:
+                X_backround = df.sample(n=self.max_samples, random_state=42)
+            else:
+                X_backround = df
+
+            if self.max_samples > 0 and len(df) > self.max_samples:
+                # Randomly sample a subset of data points if the dataset is larger than max_samples
+                self.logger.info(
+                    f"Dataset contains {len(df)} data points and max_samples is set to"
+                    f" {self.max_samples}."
+                )
+                self.logger.info(f"Sampling {self.max_samples} data points from the dataset.")
+                df = df.sample(n=self.max_samples, random_state=42)
+
+            drop_columns = (
+                data._drop_columns + ["predictions"] if data._drop_columns else ["predictions"]
             )
+            drop_columns = [col for col in drop_columns if col in df.columns]
 
-        df = data.flow
+            df = df.drop(columns=drop_columns)
+            X_backround = X_backround.drop(columns=drop_columns + [target])
 
-        if len(df) > self.max_samples:
-            # Randomly sample a subset of data points if the dataset is larger than max_samples
-            self.logger.info(
-                f"Dataset contains {len(df)} data points and max_samples is set to"
-                f" {self.max_samples}."
+            X = df.drop(columns=[target])
+            y = df[target]
+
+            explainer = RegressionExplainer(
+                model,
+                X_background=X_backround,
+                X=X,
+                y=y,
             )
-            self.logger.info(f"Sampling {self.max_samples} data points from the dataset.")
-            df = df.sample(n=self.max_samples, random_state=42)
-
-        drop_columns = (
-            data._drop_columns + ["predictions"] if data._drop_columns else ["predictions"]
-        )
-
-        df = df.drop(columns=drop_columns)
-
-        X_test = df.drop(columns=[target])
-        y_test = df[target]
-
-        explainer = RegressionExplainer(
-            model,
-            X_test,
-            y_test,
-        )
-
-        data.explainer = explainer
+            explainer.calculate_properties()
+            data.explainer = explainer
 
         return data
--- pipeline_lib/core/steps/tabular_split.py
+++ pipeline_lib/core/steps/tabular_split.py
@@ -1,58 +1,10 @@
-from typing import Iterable, Optional
+from typing import Optional
 
-import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from pipeline_lib.core import DataContainer
 from pipeline_lib.core.steps.base import PipelineStep
-
-
-def _concatenate_columns(
-    df: pd.DataFrame,
-    cols: Iterable[str],
-    sep: str = " ",
-    na_rep: str = "",
-) -> pd.Series:
-    """
-    Concatenates the specified `cols` of a DataFrame into a single column.
-
-    This function fills NaN values with a specified string, converts the columns to string type,
-    concatenates them with a specified separator and returns the concatenated values.
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        The DataFrame to process.
-    cols : iterable of str or None, optional
-        An iterable of column names to concatenate. If None, will choose all columns like `into_col`
-        with `df.filter`. Default is None.
-    sep : str, optional
-        The separator to use when concatenating. Default is " ".
-    na_rep : str, optional
-        The string to use to replace NaN values. Default is "".
-
-    Returns
-    -------
-    pandas.DataFrame
-        The processed DataFrame, which includes the new column and excludes the original columns.
-
-    Examples
-    --------
-    >>> df = pd.DataFrame({
-    ...     'A': ['a', 'b', np.nan],
-    ...     'B': ['c', np.nan, 'd'],
-    ...     'C': ['e', 'f', 'g']
-    ... })
-    >>> concatenate_columns(df, ['A', 'B', 'C'])
-       D
-    0  a c e
-    1  b f
-    2  d g
-    """
-    to_concat = df[cols].fillna(na_rep).astype(str)
-    concatted = to_concat.iloc[:, 0].str.cat(others=to_concat.iloc[:, 1:], sep=sep)
-
-    return concatted
+from pipeline_lib.utils.string_utils import concatenate_columns
 
 
 class TabularSplitStep(PipelineStep):
@@ -114,7 +66,7 @@ class TabularSplitStep(PipelineStep):
             )
 
         if self.group_by_columns is not None:
-            concatted_groupby_columns = _concatenate_columns(df, self.group_by_columns)
+            concatted_groupby_columns = concatenate_columns(df, self.group_by_columns)
             split_values = concatted_groupby_columns.unique().tolist()
         else:
             concatted_groupby_columns = None
@@ -177,7 +129,7 @@ class TabularSplitStep(PipelineStep):
 
         train_rows = len(data.train)
         validation_rows = len(data.validation)
-        test_rows = len(data.test) if test_values is not None or data.test is not None else 0
+        test_rows = len(data.test) if data.test is not None else 0
 
         total_rows = train_rows + validation_rows + test_rows
 
