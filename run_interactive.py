73bbfbc3654f2b375c213e57e9b4f2d3e77d2215
Pablo Alfaro <pabloalfaropineyro@gmail.com>
2024-06-25 14:34:07 -0300

    Add interactive runner script

---
+++ run_interactive.py
@@ -0,0 +1,33 @@
+# %%
+import logging
+
+from explainerdashboard import ExplainerDashboard
+
+from ml_garden import Pipeline
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+# %%
+# Path to your experiment configuration json file
+# json_path = "examples/ames_housing/configs/1_ames_housing_baseline.json"
+# json_path = "examples/ames_housing/configs/2_ames_housing_hp_tuning.json"
+json_path = "examples/ames_housing/configs/3_ames_housing_hp_tuned.json"
+is_train = True
+
+
+# %%
+# Load and run the pipeline using the provided JSON file path
+pipeline = Pipeline.from_json(json_path)
+data = pipeline.run(is_train=is_train)
+
+# %%
+# Load and run the pipeline using the provided JSON file path
+pipeline = Pipeline.from_json(json_path)
+data = pipeline.run(is_train=False)
+
+# %%
+data.predictions
+
+# %%
+ExplainerDashboard(explainer=data.explainer).run()
